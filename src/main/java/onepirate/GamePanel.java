6bcbc8fca01770afdfcad4711201329a29b8598e
megaseves <megaseves@gmail.com>
2023-08-11 18:10:07 +0200

    Add: game running with fps counter

--- src/main/java/onepirate/GamePanel.java
+++ src/main/java/onepirate/GamePanel.java
@@ -13,43 +13,110 @@ public class GamePanel extends JPanel implements Runnable {
     final int screenWidth = tileSize * maxScreenCol; // 768 pixels
     final int screenHeight = tileSize * maxScreenRow; // 576 pixels
 
+    int FPS = 60;
+    KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+
+
+    // Set player default position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
-
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
     }
 
     public void startGameThread() {
         gameThread = new Thread(this);
         gameThread.start();
     }
-
+/*
     @Override
     public void run() {
+
+        double drawInterval = 1000000000/FPS; // 0.0166 seconds
+        double nextDrawTime = System.nanoTime() + drawInterval;
+
         while (gameThread != null) {
-            System.out.println("The game is running...");
 
             update();
-
             repaint();
+
+            try {
+                double remainingTime = nextDrawTime - System.nanoTime();
+                remainingTime = remainingTime/1000000;
+
+                if(remainingTime < 0) {
+                    remainingTime = 0;
+                }
+
+                Thread.sleep((long) remainingTime);
+
+                nextDrawTime += drawInterval;
+
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }*/
+    @Override
+    public void run() {
+        double drawInterval = 1000000000/FPS; // 0.0166 seconds
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer = 0;
+        int drawCount = 0;
+
+        while(gameThread != null) {
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime) / drawInterval;
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
+
+            if(delta >= 1) {
+                update();
+                repaint();
+                delta--;
+                drawCount++;
+            }
+
+            if(timer >= 1000000000) {
+                System.out.println("FPS: " + drawCount);
+                drawCount = 0;
+                timer = 0;
+            }
         }
     }
 
-    public void update() {
 
+    public void update() {
+        if (keyH.upPressed) {
+            playerY -= playerSpeed;
+        } else if (keyH.downPressed) {
+            playerY += playerSpeed;
+        } else if (keyH.rightPressed) {
+            playerX += playerSpeed;
+        } else if (keyH.leftPressed) {
+            playerX -= playerSpeed;
+        }
     }
 
     public void paintComponent(Graphics g) {
+        Toolkit.getDefaultToolkit().sync();
         super.paintComponent(g);
 
         Graphics2D g2 = (Graphics2D)g;
 
         g2.setColor(Color.white);
 
-        g2.fillRect(100, 100, tileSize, tileSize);
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
 
         g2.dispose();
     }
--- src/main/java/onepirate/Main.java
+++ src/main/java/onepirate/Main.java
@@ -5,15 +5,13 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         JFrame window = new JFrame();
+        GamePanel gamePanel = new GamePanel();
+
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
         window.setTitle("One Pirate");
-
-        GamePanel gamePanel = new GamePanel();
         window.add(gamePanel);
-
         window.pack();
-
         window.setLocationRelativeTo(null);
         window.setVisible(true);
 
